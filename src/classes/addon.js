938b8cb73377c36863c6ef4e5091be4b303bbc83
mount2010 <13136505+mount2010@users.noreply.github.com>
2019-12-01 21:54:23 +0800

    Refactor Addons command

---
+++ src/classes/addon.js
@@ -0,0 +1,46 @@
+const config = require("../../config/config.json");
+const utils = require("../util");
+
+class Addon {
+	constructor (data) {
+		this.name = data.name;
+		this.desc = data.desc;
+		this.url = data.url;
+		this.author = data.author;
+		this.porter = data.porter;
+		this.dependencies = data.dependencies;
+		this.deprecated = data.deprecated;
+		this.type = data.type ? data.type : "Addon";
+	}
+	details () {
+		const embed = utils.embed("success");
+		embed.setTitle(this.name);
+		embed.setDescription(this.desc);
+		embed.addField("Original Author", this.author);
+		embed.setURL(this.url);
+		if (this.porter && this.author !== this.porter) {
+			embed.addField("Porter", this.porter);
+		}
+		if (this.dependencies) {
+			embed.addField("Dependencies", this.dependencies);
+		}
+		if (this.deprecated) {
+			embed.addField("Deprecated", this.deprecated);
+		}
+		embed.addField("Type", this.type ? this.type : "Addon");
+		return embed;
+	}
+	icons () {
+		const icons = config.icons;
+		const type = this.type === "Resource Pack" ? icons.resourcePack : icons.addon;
+		const deprecated = this.deprecated ? icons.deprecated : "";
+		const dependencies = this.dependencies ? icons.dependencies : "";
+		return { type, deprecated, dependencies };
+	}
+	field () {
+		const icons = this.icons();
+		return `[${this.name}](${this.url}) ${icons.type} ${icons.deprecated} ${icons.dependencies}`;
+	}
+}
+
+module.exports = Addon;
--- src/commands/addons.js
+++ src/commands/addons.js
@@ -1,21 +1,33 @@
-// Todo: refactor and split this file into functions
-const addons = require("../../data/addons.json");
-const recommends = require("../../data/recommends.json");
-const utils = require("../util");
 const levenshtein = require("fast-levenshtein");
 const { Collection } = require("discord.js");
 
-// Acceptable distance for typos
-const maxDistance = 3;
-// Icons
-const resourcePackIcon = ":frame_photo:";
-const deprecatedIcon = ":boom:";
-const dependenciesIcon = ":gear:";
-const addonIcon = ":heavy_plus_sign:";
+const Addon = require("../classes/addon");
+const utils = require("../util");
+
+const config = require("../../config/config.json");
+const addons = require("../../data/addons.json").map(el=>{return new Addon(el)});
+const recommends = require("../../data/recommends.json").map(el=>{return new Addon(el)});
+
+const sortByRow = new utils.SortByRow(addons);
+const porters = sortByRow.allRelatedRowsFor("porter");
+let collection = new Collection(addons.map(el => [el.name, el]));
+
+const recommendedList = [
+	"Recommended",
+	recommends.map(i=>i.field()).join('\n'),
+	true
+];
+const addonsList = porters.map(el=>{
+	return [
+		el.similar,
+		el.vals.map(i=>i.field()).join('\n'),
+		true
+	];
+})
+addonsList.push(recommendedList);
 
 function details(client, msg, args) {
 	const sortByRow = new utils.SortByRow(addons);
-	let collection = new Collection(addons.map(el => [el.name, el]));
 	recommends.forEach(el => {
 		collection.set(el.name, el);
 	});
@@ -31,34 +43,20 @@ function details(client, msg, args) {
 	// Only use values that match the first word of the msg. Uses levenshtein to account for typos
 	collection = collection.filter(el => {
 		const distance = levenshtein.get(el.name.toLowerCase(), query);
-		return distance <= maxDistance;
+		return distance <= config.addons.maxTypos;
 	});
-	if (collection.size < 1) {
-		// No addon found, try porter:
-		const porter = sortByRow.getRelatedRows("porter", args[0], true);
-		if (porter.length < 1) {
-			msg.channel.send(
-				utils
-					.embed("error")
-					.setTitle("No addon found")
-					.setDescription(
-						`Couldn't find an addon with that name. Use ${client.prefix}addons to see a list of all addons.`
-					)
-			);
-			return;
-		} else {
-			const embed = utils
-				.embed("success")
-				.setTitle("Porter listing")
-				.setDescription(`Ports/addons by ${query}`);
-			porter.forEach(el => {
-				embed.addField(el.name, `[Link](${el.url}) - ${el.desc}`);
-			});
-			msg.channel.send(embed);
-			return;
-		}
+	if (!collection.size) {
+		msg.channel.send(
+			utils
+				.embed("error")
+				.setTitle("No addon found")
+				.setDescription(
+					`Couldn't find an addon with that name. Use ${client.prefix}addons to see a list of all addons.`
+				)
+		);
+		return;
 	}
-	if (collection.size > 1) {
+	else if (collection.size > 1) {
 		msg.channel.send(
 			utils
 				.embed("error")
@@ -70,70 +68,21 @@ function details(client, msg, args) {
 		return;
 	} else match = collection.values().next().value;
 
-	const embed = utils.embed("success");
-	embed.setTitle(match.name);
-	embed.setDescription(match.desc);
-	embed.addField("Original Author", match.author);
-	embed.setURL(match.url);
-	if (match.porter && match.author !== match.porter) {
-		embed.addField("Porter", match.porter);
-	}
-	if (match.dependencies) {
-		embed.addField("Dependencies", match.dependencies);
-	}
-	if (match.deprecated) {
-		embed.addField("Deprecated", match.deprecated);
-	}
-	embed.addField("Type", match.type ? match.type : "Addon");
-	msg.channel.send(embed);
+	msg.channel.send(match.details());
 }
 
 function list(client, msg, args) {
-	// Cache this to reduce runtime. For now, not needed
-	const sortByRow = new utils.SortByRow(addons);
-	const porters = sortByRow.allRelatedRowsFor("porter");
 	const embed = utils.embed("success").setTitle("TFC+ Addons");
 	embed.setDescription(
-		"For details, supply an author or addon name as an argument." +
-			"\n" +
-			`Legend: ${resourcePackIcon} Resource Pack | ${addonIcon} Addon | ${deprecatedIcon} Deprecated | ${dependenciesIcon} Has Dependencies`
+		"**For details, supply an addon name as an argument.**\n" +
+		`Legend: ${config.icons.resourcePack} Resource Pack | ${config.icons.addon} Addon 
+		| ${config.icons.deprecated} Deprecated | ${config.icons.dependencies} Has Dependencies (use ${config.prefix}addons <name>)`
 	);
 
-	function addonIcons(obj) {
-		const type =
-			obj.type === "Resource Pack" ? resourcePackIcon : addonIcon;
-		const deprecated = obj.deprecated ? deprecatedIcon : "";
-		const dependencies = obj.dependencies ? dependenciesIcon : "";
-		return { type, deprecated, dependencies };
-	}
-
-	const fields = porters.map(el => {
-		return [
-			`${el.similar}:`,
-			el.vals
-				.map(i => {
-					const icons = addonIcons(i);
-
-					return `[${i.name}](${i.url}) ${icons.type} ${icons.deprecated} ${icons.dependencies}`;
-				})
-				.join("\n"),
-			true
-		];
-	});
-	const recommendsField = [
-		"Recommended:",
-		recommends.map(i => {
-			const icons = addonIcons(i);
-
-			return `[${i.name}](${i.url}) ${icons.type} ${icons.deprecated} ${icons.dependencies}`;
-		}),
-		true
-	];
-
-	fields.forEach(el => embed.addField(...el));
-	embed.addField(...recommendsField);
+	addonsList.forEach(el => embed.addField(...el));
 	msg.channel.send(embed);
 }
+
 
 module.exports.run = function(client, msg, args) {
 	if (args.length >= 1) {
